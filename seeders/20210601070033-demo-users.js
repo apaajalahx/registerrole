5b83d5f4b6a85c321b73a87d52fe94399909ccb2
Dinar Hamid <dinar@xaisyndicate.co.id>
2021-06-01 14:06:21 +0700

    add seeder data using sequelize cli

---
+++ seeders/20210601070033-demo-users.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcryp = require('bcryptjs');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('users', [{
+        name: "admin",
+        email: "admin@example.com",
+        phone: "081288888",
+        password: bcryp.hashSync("admin123"),
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('users',null, {});
+  }
+};
